b3d3d1f1a21198abe80eac2dc736ebf15c0b06dc
bootcampwill <william.seymour128@gmail.com>
2020-01-29 09:24:35 -0500

    removed api
    removed api

--- logic_0.js
+++ logic_0.js
@@ -9,7 +9,7 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
   id: "mapbox.streets",
-  accessToken: "pk.eyJ1IjoiYm9vdGNhbXB3aWxsIiwiYSI6ImNrNWcydzg5czAzbmMzbW85ZTIwbndja20ifQ.6kgo91XHQcOfz0_AwfBjfQ"
+  accessToken: API_KEY
 }).addTo(map);
 
 //Connecting to the endpoint
