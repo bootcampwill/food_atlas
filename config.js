057c2499d04d783f64f2fe9eab60dbd4a935a462
nswehli <53152702+nswehli@users.noreply.github.com>
2020-02-04 14:19:20 -0500

    Project updates

---
+++ config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibnN3ZWhsaSIsImEiOiJjazVnMnc2ZHowM244M2pxbTFlYWhzMXVwIn0.0CxW_QdppTZUjpTaUh8-dQ";
---
+++ config.py
@@ -0,0 +1,5 @@
+db_endpoint = 'foodatlas.cvc7tm9vqggx.us-east-2.rds.amazonaws.com'
+db_username = 'postgres'
+db_password = 'nacRackE'
+db_port = 5432
+db_name = 'foodAtlas'
